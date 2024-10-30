e285098a8931accb056dee36a4225742f09cb71f
ejmockler <emockler@ucsc.edu>
2024-10-29 23:19:26 -0700

    fix footer on small screens

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="mb-8 mx-12">
+<footer class="mt-auto mb-8 mx-12">
     <div class="border-t-2 border-gray-400 pt-8">
         <p class="text-base text-gray-400 text-center">
             © {new Date().getFullYear()} Next Generation Analytics. All rights reserved.
--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -4,15 +4,16 @@
     import { teamMembers } from '$lib/data/team';
 </script>
 
-<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2" style="height: calc(100vh - 5rem)">
+<div class="min-h-[calc(100vh-5rem)] flex flex-col w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2">
     <div class="text-center small:mb-12 small:mt-0 mt-4 mb-6">
         <h1 class="text-4xl font-extrabold text-gray-900 sm:text-5xl">
             Our Team
         </h1>
         <p class="mt-4 text-xl text-gray-500">
-          Meet the experts shaping next-gen solutions in computational biology.
+            Meet the experts shaping next-gen solutions in computational biology.
         </p>
     </div>
 
     <TeamGrid members={teamMembers} />
+    <div class="flex-grow"></div>
 </div>
